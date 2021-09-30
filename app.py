0bc2a7f76855155eea90135691ba675d09af4f1d
Gabriel <gabriel.schallenberger@outlook.com>
2021-09-30 19:14:33 +0200

    zz

--- app.py
+++ app.py
@@ -39,65 +39,8 @@ def save(data):
 def sma(data, n):
 	sma = data.rolling(window=n).mean()
 	return pd.DataFrame(sma)
-def s1(data, short_window, long_window,joker,budget_l):
-	
-	sma0 = joker
-	sma1 = short_window
-	sma2 = long_window
-	buy_price = []
-	sell_price = []
-	sma_signal = []
-	last_signal = 0
-	signal = -1
-	profit = budget_l
-	trade = []
-	dispo = 0
 
-	for i in range(len(data)):
-		if sma1[i] > sma2[i]:
-			if signal != 1: #
-				buy_price.append(data[i])
-				last_signal = data[i]
-				print("buy : ",data[i])
-				sell_price.append(np.nan)
-				signal = 1
-				dispo = profit/data[i]
-				profit =profit-budget_l
-				print(profit)
-				sma_signal.append(signal)
-			else:
-				buy_price.append(np.nan)
-				sell_price.append(np.nan)
-				sma_signal.append(0)
-		elif sma2[i] > sma1[i]:
-			if signal != -1:
-				buy_price.append(np.nan)
-				if(last_signal>data[i]):
-					sell_price.append(np.nan)
-					sma_signal.append(0)
-				else:
-					sell_price.append(data[i])
-					print("sell : ",data[i])
-					profit = dispo*data[i]
-					print(profit)
-					dispo = 0
-					trade.append(profit-budget_l)
-					signal = -1
-					sma_signal.append(-1)
-			else:
-				buy_price.append(np.nan)
-				sell_price.append(np.nan)
-				sma_signal.append(0)
-		else:
-			buy_price.append(np.nan)
-			sell_price.append(np.nan)
-			sma_signal.append(0)
-		
-	return buy_price, sell_price, sma_signal,trade,(profit)
-
-
-def s2(data, short_window, long_window,joker,budget_l):	
-	print("s2")
+def s2(data, short_window, long_window,joker,budget_l,nb_trade,ad):	
 	sma0 = joker
 	sma1 = short_window
 	sma2 = long_window
@@ -110,9 +53,8 @@ def s2(data, short_window, long_window,joker,budget_l):
 	last_sell = []
 
 	last_signal = 0
-	max_signal = 2
-	signal = max_signal
-	slic=budget_l/2
+	signal = nb_trade
+	slic=budget_l/nb_trade
 	profit = budget_l
 	trade = []
 	dispo = 0
@@ -123,20 +65,18 @@ def s2(data, short_window, long_window,joker,budget_l):
 				buy_price.append(data[i])
 				last_buy.append(data[i])
 				last_signal = data[i]
-				#print("buy : ",data[i]," dispo : ",dispo+slic/data[i])
 				sell_price.append(np.nan)
 				dispo += slic/data[i]
 				signal-=1
-				# print(profit)
 				sma_signal.append(signal)
 			else:
 				buy_price.append(np.nan)
 				sell_price.append(np.nan)
 				sma_signal.append(0)
 		elif sma2[i] > sma1[i]:
-			if signal < max_signal:
+			if signal < nb_trade:
 				buy_price.append(np.nan)
-				if(last_signal>data[i]):
+				if(max(last_buy)>data[i]):
 					sell_price.append(np.nan)
 					sma_signal.append(0)
 				else:
@@ -145,9 +85,11 @@ def s2(data, short_window, long_window,joker,budget_l):
 					last_sell.append(data[i])
 					signal+=1
 					dispo -= slic/min(last_buy)
-					trade.append(profit-budget_l)
 					sma_signal.append(-1)
 					la = last_buy.pop(-1)
+					trade.append(((slic/la)*data[i])-slic)
+					if(ad==1):
+						slic=(slic/la)*data[i]
 					if(len(last_buy)==0):
 						print("buy at : ",last_signal," sell at : ",data[i]," for (",slic/la,") :",(slic/la)*data[i]," profit : ",profit-budget_l)
 					else:
@@ -165,6 +107,7 @@ def s2(data, short_window, long_window,joker,budget_l):
 
 @app.route("/<version>/<name>")
 def currency(version,name):
+
 	save(req(name,30))
 	data = pd.read_csv('tst.csv').set_index('Date')
 	data.index = pd.to_datetime(data.index)
@@ -177,17 +120,18 @@ def currency(version,name):
 	sma_20 = data['sma_20']
 	sma_50 = data['sma_50']
 
-	if(version=="1"):
-		print("good")
-		buy_price, sell_price, signal,trade,profit =s1(data['Close'], sma_20, sma_10, sma_50,budget_l)
-	else:
-		buy_price, sell_price, signal,trade,profit =s2(data['Close'], sma_20, sma_10, sma_50,budget_l)
+	switch={
+      "1":s2(data['Close'], sma_20, sma_10, sma_50,budget_l,1,0),
+      "2":s2(data['Close'], sma_20, sma_10, sma_50,budget_l,2,0),
+	  "3":s2(data['Close'], sma_20, sma_10, sma_50,budget_l,2,1)
+      }
+    
+	buy_price, sell_price, signal,trade,profit = switch.get(version,"Invalid input")
 
 	f = plt.figure()
 	f.set_figwidth(15)
 	f.set_figheight(7)
-	# print(version)
-	# print(buy_price,sell_price)
+
 	plt.plot(data['Close'], alpha = 0.3, label = 'data')
 	plt.plot(sma_10, alpha = 0.6, label = 'SMA 10')
 	plt.plot(sma_20, alpha = 0.6, label = 'SMA 20')
@@ -195,12 +139,14 @@ def currency(version,name):
 	plt.scatter(data.index, buy_price, marker = '^', s = 200, color = 'darkblue', label = 'BUY SIGNAL')
 	plt.scatter(data.index, sell_price, marker = 'v', s = 200, color = 'crimson', label = 'SELL SIGNAL')
 	plt.legend(loc = 'upper left')
+
 	if(len(trade)!=0):
-		title = 'SMA 10-20 reverse cross '+version+' \n currency : '+name+' budget : '+str(budget_l)+'\n profit :'+str(profit)+'\n avg profit : '+str(sum(trade)/len(trade))
-	else: title = 'SMA 10-20 reverse cross '+version+' \n currency : '+name+' budget : '+str(budget_l)+'\n profit : niet nada'
-	plt.title(title)
+		title = 'profit :'+str(profit)+'\n avg profit : '+str(sum(trade)/len(trade))
+	else: title = 'profit : niet nada'
+
+	plt.title('SMA 10-20 reverse cross \n currency : '+name+' budget : '+str(budget_l))
 	buf = BytesIO()
 	plt.savefig(buf, format="png")
-	# Embed the result in the html output.
+
 	dat = base64.b64encode(buf.getbuffer()).decode("ascii")
-	return f"<img src='data:image/png;base64,{dat}'/>"
+	return f"<p>{title}</p><p>{trade}</p><img src='data:image/png;base64,{dat}'/>"
