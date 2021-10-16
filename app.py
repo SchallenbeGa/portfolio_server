7130d514093924ed255f0bf574dbd9b77ddb6fdf
Gabriel <gabriel.schallenberger@outlook.com>
2021-10-16 12:20:50 +0200

    edit/trash/new

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 
 import pandas as pd
 from flask import Flask
-from flask import render_template,redirect
+from flask import render_template,redirect,request
 
 from pycoingecko import CoinGeckoAPI
 
@@ -29,19 +29,77 @@ def save(name,days,pat):
 			this_csv_file.write(line)
 			this_csv_file.write('\n')
 
+def save_budget(data):
+	line_list=[]
+	with open("budget-example.csv", 'w') as this_csv_file:
+		line_list.append('name,hold,buy_price')
+		for y in data:
+			print(y["money"])
+			line= str(y["money"])+","+str(y["quantity"])+","+str(y["buy_price"])
+			line_list.append(line)
+		for line in line_list:
+			this_csv_file.write(line)
+			this_csv_file.write('\n')
+
 @app.route("/edit")
 def edit():
+	currencies = pd.read_csv('budget-example.csv')
 	trade = []
-
 	for i,z,buy in currencies.values:
 		tr = {"money":i,"quantity":z,"buy_price":buy}
 		trade.append(tr)
 
 	return render_template("edit.html",trade=trade)
 
+@app.route('/edit',methods = ['POST'])
+def save_d():
+
+	money = request.form["money"]
+	buy_price = request.form['buy_price']
+	quantity = request.form['quantity']
+
+	trade = []
+	for i,z,buy in currencies.values:
+		if(i==money):
+			i = money
+			z = quantity
+			buy = buy_price
+		trade.append({"money":i,"quantity":z,"buy_price":buy})
+	save_budget(trade)
+
+	return redirect('edit')
+
+@app.route('/trash',methods = ['POST'])
+def trash():
+	
+	money = request.form["money"]
+
+	trade = []
+	for i,z,buy in currencies.values:
+		if(i!=money):
+			trade.append({"money":i,"quantity":z,"buy_price":buy})
+	save_budget(trade)	
+
+	return redirect('edit')
+
+@app.route('/new',methods = ['POST'])
+def new():
+	
+	money = request.form["money"]
+	buy_price = request.form['buy_price']
+	quantity = request.form['quantity']
+
+	trade = []
+	for i,z,buy in currencies.values:
+		trade.append({"money":i,"quantity":z,"buy_price":buy})
+	trade.append({"money":money,"quantity":quantity,"buy_price":buy_price})
+	save_budget(trade)	
+
+	return redirect('edit')
+
 @app.route("/")
 def home():
-
+	currencies = pd.read_csv('budget-example.csv')
 	for n in 1,30:
 		for i,z,buy in currencies.values:
 			pat = 'data/'+i+'_'+str(n)+'.csv'
@@ -80,9 +138,10 @@ def home():
 				comment = "may buy"
 				if (data_p['Close'].iloc[-1]<sma_10) & (data_p['Close'].iloc[-1]<sma_20) & (data_p['Close'].iloc[-1]<sma_30)  :
 					comment = "BUY !!!"
-	
-		# print("money : ",i," price : ",data_p['Close'].iloc[-1],"sma 10 : ",sma_10,"sma 20 : ",sma_20,"sma 30 : ",sma_30," advice : ",comment)
-		tr = {"money":i,"quantity":z,"price":data_p['Close'].iloc[-1],"buy_price":buy,"sma_10":sma_10,"sma_20":sma_20,"sma_30":sma_30,"comment":comment}
+		
+		pnl = (z*data_p['Close'].iloc[-1])-(z*buy)
+		
+		tr = {"pnl":pnl,"money":i,"quantity":z,"price":data_p['Close'].iloc[-1],"buy_price":buy,"sma_10":sma_10,"sma_20":sma_20,"sma_30":sma_30,"comment":comment}
 		trade.append(tr)
 
 	return render_template("index.html",trade=trade)
