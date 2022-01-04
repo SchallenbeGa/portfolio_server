c16050c8aab09c1a00e5ec09a37fd3a126e7c7f2
schallenbega <gabriel.schallenberger@outlook.com>
2022-01-04 18:25:32 +0100

    wip

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from datetime import datetime
 SOCKET = "wss://stream.binance.com:9443/ws/xrpusdt@kline_1m"
 
 TRADE_SYMBOL = 'XRPUSDT'
-TRADE_QUANTITY = 20
+TRADE_QUANTITY = 50
 
 order_id = 0
 in_position = False
@@ -16,6 +16,13 @@ async def save(tim,data):
         contents = await f.read()
         contents = contents+"\n"+str(datetime.utcfromtimestamp(tim/1000))+","+str(data['o'])+","+str(data['h'])+","+str(data['l'])+","+str(data['c'])
     async with aiofiles.open('tst.csv', mode='w') as f:
+        await f.write(contents)
+
+async def save_trade(b_s,price):
+    async with aiofiles.open('trade.csv', mode='r') as f:
+        contents = await f.read()
+        contents = contents+"\n"+str(str(b_s)+","+str(price))
+    async with aiofiles.open('trade.csv', mode='w') as f:
         await f.write(contents)
 
 client = Client(config.API_KEY, config.API_SECRET, tld='com')
@@ -59,25 +66,30 @@ def on_message(ws, message):
     data = pd.read_csv('tst.csv').set_index('Date')
     data.index = pd.to_datetime(data.index)
 
-    # get price avg for 5-10-30 days
-    sma_5 = pd.DataFrame(data['Close'].rolling(window=10).mean()).max()['Close']
-    sma_10 = pd.DataFrame(data['Close'].rolling(window=20).mean()).max()['Close']
-
-    data = data['Close']
-
-    if(len(data)<50):# if less than 20 trade wait
-        print("wait",len(data),"/50")
+    if(len(data)<30):# if less than 20 trade wait
+        print("wait",len(data),"/20")
     else:
+        # get price avg for 5-10-30 days
+        #print(pd.DataFrame(data['Close'][-20:].rolling(window=10).mean()))
+        
+        # get price avg for 5-10-30 days
+        sma_5 = pd.DataFrame(data['Close'].rolling(window=10).mean()).max()['Close']
+        sma_10 = pd.DataFrame(data['Close'].rolling(window=20).mean()).max()['Close']
+
+
+        data = data['Close']
         print("current price :",data[-1])
         print("avg last 10 trade :",sma_5)
         print("avg last 20 trade :",sma_10)
       
-        if data[-1] < sma_5: # if last price < last 10 trade's price avg = buy
+        if data[-1] > sma_5: # if last price < last 10 trade's price avg = buy
             if in_position == False:
                 order_succeeded = order(0,SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
                 if order_succeeded:
                     in_position = True
-                    order_sell = order(round(data[-1]+data[-1]*0.0007,4),SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL) # sell at : buy price + 0.0007%
+                    asyncio.run(save_trade("buy",data[-1]))
+                    print('\a')
+                    order_sell = order(round(data[-1]+data[-1]*0.0002,4),SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL) # sell at : buy price + 0.0007%
                     if order_sell:
                         print("success sell limit")
                     else:
@@ -87,8 +99,13 @@ def on_message(ws, message):
             else:
                 sorder = client.get_order(symbol=TRADE_SYMBOL,orderId=order_id)
                 if sorder['status'] == 'FILLED':
-                    print("restart please")
-                    in_position = False
+                    print("wait for restart")
+                    if(data[-1]<sma_10):
+                        print(sorder['price'])
+                        asyncio.run(save_trade("sell",sorder['price']))
+                        print('\a')
+                        print('\a')
+                        in_position = False
                 else:  
                     print("waiting for sell : ",sorder)
 
