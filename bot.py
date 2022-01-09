8281ffc5d0ff638713cae1d1eab510a999290f8f
schallenbega <gabriel.schallenberger@outlook.com>
2022-01-09 19:11:22 +0100

    twetgraph

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-import websocket, json, config, aiofiles,pandas as pd,asyncio
+import websocket, json, base64, config, aiofiles,pandas as pd,asyncio,numpy as np,mplfinance as mpf
 from binance.client import Client
 from binance.enums import *
 from datetime import datetime
-from dateutil import tz
+from io import BytesIO
 
 #https://developer.twitter.com/
 #https://developer.twitter.com/en/apps
@@ -14,10 +14,10 @@ from dateutil import tz
 #save
 import tweepy
 
-consumer_key = 'nGZ2GOUnmJhsecretagsdHDKG'
-consumer_secret = 'Yc7iFcuDFlNxQsecretdOGjFjgy'
-access_token = '126528594162481152secretcjINAvSFWmHLCFcqHfHSQPKplJZ'
-access_token_secret = 'NkvqRUgBuhX9xSAjCDWsecret2EHO3S640KOIxDgCaVuGC'
+consumer_key = 'nGZ2GOUt5ZagsdHDKG'
+consumer_secret = 'Yc7iFcuDFlNxQtFLEbEapItOUO9aWaH931VwRdOGjFjgy'
+access_token = '1265285941624811520-tmHLCFcqHfHSQPKplJZ'
+access_token_secret = 'NkvqRUgBuhXtWQWP2j2EHO3S640KOIxDgCaVuGC'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -30,6 +30,8 @@ TRADE_SYMBOL = config.PAIR_M
 TRADE_QUANTITY = config.QUANTITY
 TEST = config.DEBUG
 
+sma_d = 8
+added_val = 0.002
 order_id = 0
 in_position = False
 sell_price = 0
@@ -48,6 +50,105 @@ with open("trade.csv", "w") as f:
 
 with open("order.csv", "w") as f:
     f.write("Date,Type,Price,Quantity\n")
+
+
+##tweet graph img
+def twet_graph(tweet_content):
+    global api,sma_d
+    print("start graph")
+    data = pd.read_csv('tst.csv').set_index('Date')
+    data.index = pd.to_datetime(data.index,format="%Y-%m-%d %H:%M:%S")
+    trade = pd.read_csv('trade.csv')
+    trade_d = trade
+    trade.set_index('Date')
+    buf = BytesIO()
+    fees = []	
+    gains = []
+    s  = mpf.make_mpf_style(base_mpf_style="yahoo",facecolor="#282828",gridcolor="#212121",rc={'xtick.color':'#f8f8f8','ytick.color':'#f8f8f8','axes.labelcolor':'#f8f8f8'})
+    if len(trade)>0 :
+        
+        buy = []
+        sell = []
+        trade_f = []
+        for x in range(len(trade)):
+            if len(trade)>1:
+                if(x<=len(trade)-2):
+                    print(trade['Price'][x])
+                    gains.append(trade['Price'][x+1]*int(config.QUANTITY)-trade['Price'][x]*int(config.QUANTITY))
+            fees.append(((trade['Price'][x]*int(config.QUANTITY))/100)*0.1)
+            trade_f.append(datetime.strptime(trade_d['Date'][x],"%Y-%m-%d %H:%M:%S").minute)
+
+        for i in range(len(data)):
+            is_done = False
+            if data.index.array[i].minute in trade_f:
+                print("maybe")
+                for x in range(len(trade)):
+                    if((data.index.array[i].minute==datetime.strptime(trade_d['Date'][x],"%Y-%m-%d %H:%M:%S").minute) & (data.index.array[i].hour==datetime.strptime(trade_d['Date'][x],"%Y-%m-%d %H:%M:%S").hour)):
+                        print("start")
+                        is_done = True
+                        print(data['Low'][i])
+                        print(trade['Price'][x])	
+                        if(trade['Type'][x]=="buy"):
+                            buy.append(trade['Price'][x]-0.0005)
+                        else:
+                            sell.append(trade['Price'][x]+0.0005)
+            if not is_done:
+                buy.append(np.nan)
+                sell.append(np.nan)
+        if len(trade)>1:
+            if len(trade)%2 != 0:
+                sell.append(np.nan)
+        for x in range(int((len(trade)-2)/2)):
+            buy.append(np.nan)
+            sell.append(np.nan)
+
+        if len(data)>len(buy):
+            buy.append(np.nan)
+            sell.append(np.nan)
+        
+        sell_n = 0
+        for v in sell :              
+            if not np.isnan(v):     
+                sell_n+=1
+
+        if sell_n>0:
+            apd = [
+                mpf.make_addplot(buy, scatter=True, markersize=200, marker=r'^', color='green'),
+                mpf.make_addplot(sell, scatter=True, markersize=200, marker=r'v', color='red')
+            ]
+        else:
+            apd = [mpf.make_addplot(buy, scatter=True, markersize=200, marker=r'^', color='green')]
+            print("py")
+        fig,ax = mpf.plot(
+            data,
+            addplot=apd,
+            type='candle',
+            volume=True,
+            style=s,
+            mav=(8),
+            figscale=1,
+            figratio=(20,10),
+            datetime_format="%d %H:%M:%S",
+            xrotation=0,
+            returnfig=True)
+        fig.savefig('tweettest.png',facecolor='#282828')
+    else:
+        fig,ax = mpf.plot(
+            data,
+            type='candle',
+            volume=True,
+            style=s,
+            mav=(8),
+            figratio=(20,10),
+            datetime_format="%d %H:%M:%S",
+            xrotation=0,
+            returnfig=True)
+        fig.savefig('tweettest.png',facecolor='#282828')
+    print("save graph")
+    order = pd.read_csv('order.csv')
+    dat = base64.b64encode(buf.getbuffer()).decode("ascii")
+
+    api.update_status_with_media(tweet_content,"tweettest.png")
 
 #save trade form the bot in trade.csv
 async def save_trade(b_s,price):
@@ -87,7 +188,6 @@ def order(limit,side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
     global order_id
     try:
         if limit > 0:
-            print("test")
             order = client.create_order(symbol=symbol,side=side,type=ORDER_TYPE_LIMIT,quantity=TRADE_QUANTITY,price=limit,timeInForce=TIME_IN_FORCE_GTC)
         else:
             order = client.create_order(symbol=symbol,side=side,type=order_type, quantity=quantity)
@@ -113,7 +213,7 @@ def on_close(ws):
     print('closed connection')
 
 def on_message(ws, message):
-    global in_position,order_id,sell_price,api
+    global in_position,order_id,sell_price,api,added_val,sma_d
 
     json_message = json.loads(message)  
     candle = json_message['k']
@@ -125,14 +225,20 @@ def on_message(ws, message):
     data = pd.read_csv('tst.csv').set_index('Date')
     data.index = pd.to_datetime(data.index)
 
-    sma = data['Close'][-8:].mean()
+    sma = data['Close'][-sma_d:].mean()
     close = float(candle['c'])
 
     if (is_candle_closed):
         if in_position:
-            print("okay")
-            tweet = "buy at : "+str(sell_price-0.002)+"\nlast price : "+str(data['Close'][-1])+"\nactual profit : "+str(data['Close'][1]-(sell_price-0.002))
-            api.update_status(tweet)
+            if data['Close'][-1]-(sell_price-added_val) <0:
+                print((data['Close'][-1]*float(TRADE_QUANTITY))-((sell_price-added_val)*float(TRADE_QUANTITY)))
+                tweet = "buy at : "+str(sell_price-added_val)+"\nlast price : "+str(data['Close'][-1])+"\nactual loss : -"+str(((sell_price-added_val)*float(TRADE_QUANTITY))-(data['Close'][-1]*float(TRADE_QUANTITY)))
+            else:
+                tweet = "buy at : "+str(sell_price-added_val)+"\nlast price : "+str(data['Close'][-1])+"\nactual profit : "+str((data['Close'][-1]*float(TRADE_QUANTITY))-((sell_price-added_val)*float(TRADE_QUANTITY)))
+            tweet+="\nsell limit at : "+str(sell_price)
+            twet_graph(tweet)
+        else:
+            twet_graph("waiting... need to cross blue line\nactual price : "+str(data['Close'][-1])+"\ncross price : "+str(sma))
 
     print("current price :",close)
     print("cross price   :",sma)
@@ -140,13 +246,9 @@ def on_message(ws, message):
     if in_position:
         sorder = client.get_order(symbol=TRADE_SYMBOL,orderId=order_id)
         if TEST:
-            print("Ready")
-            print(type(sorder['price']))
-            print(type(close))
             if sell_price<=close:
-                tweet = "sell at: "+close
+                tweet = "sell at: "+str(close)+"\nprofit : "+str((close*float(TRADE_QUANTITY))-((sell_price-added_val)*float(TRADE_QUANTITY)))
                 api.update_status(tweet)
-                print("selll")
                 print("cross price :",sma)
                 print(sorder['price'])
                 asyncio.run(save_trade("sell",sorder['price']))
@@ -160,8 +262,6 @@ def on_message(ws, message):
                 print("cross price :",sma)
                 print(sorder['price'])
                 asyncio.run(save_trade("sell",sorder['price']))
-                print('\a')
-                print('\a')
                 in_position = False
             else:  
                 print("waiting for sell : ",sorder)
@@ -171,17 +271,13 @@ def on_message(ws, message):
             order_succeeded = order(0,SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
             if order_succeeded:
                 in_position = True
-                print("here")
                 tweet = "buy at: "+str(close)
                 api.update_status(tweet)
                 asyncio.run(save_trade("buy",close))
-                print("hoo")
-                print('\a')
-                sell_price = close+0.002
-                order_sell = order(close+0.002,SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL) # sell at : buy price + 0.0005%
+                sell_price = close+added_val
+                order_sell = order(close+added_val,SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL) # sell at : buy price + added_val
                 if order_sell:
-                    print("here")
-                    asyncio.run(save_order(close+0.002))
+                    asyncio.run(save_order(close+added_val))
                     print("success sell limit")
                 else:
                     print("fail sell limit")
---
+++ tst_tw.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+import mplfinance as mpf
+import base64
+import config
+from io import BytesIO
+import datetime
+
+import tweepy
+
+consumer_key = 'nGZ2GOUnmJheVWj5ZagsdHDKG'
+consumer_secret = 'Yc7iFcuDFlNxQtFLEbEapIduSsN7OUO9aWaH931VwRdOGjFjgy'
+access_token = '1265285941624811520-yCEcjINAvSFWmHLCFcqHfHSQPKplJZ'
+access_token_secret = 'NkvqRUgBuhX9xSAjCDWQWP2j2EHO3S640KOIxDgCaVuGC'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+def twet_graph():
+    data = pd.read_csv('tst.csv').set_index('Date')
+    data.index = pd.to_datetime(data.index,format="%Y-%m-%d %H:%M:%S")
+
+    trade = pd.read_csv('trade.csv')
+    trade_d = trade
+    trade.set_index('Date')
+
+    buf = BytesIO()
+    fees = []	
+    gains = []
+    s  = mpf.make_mpf_style(base_mpf_style="yahoo",facecolor="#282828",gridcolor="#212121",rc={'xtick.color':'#f8f8f8','ytick.color':'#f8f8f8','axes.labelcolor':'#f8f8f8'})
+        
+    if len(trade)>0 :
+        
+        buy = []
+        sell = []
+        trade_f = []
+        for x in range(len(trade)):
+            if len(trade)>1:
+                if(x<=len(trade)-2):
+                    print(trade['Price'][x])
+                    gains.append(trade['Price'][x+1]*int(config.QUANTITY)-trade['Price'][x]*int(config.QUANTITY))
+            fees.append(((trade['Price'][x]*int(config.QUANTITY))/100)*0.1)
+            trade_f.append(datetime.datetime.strptime(trade_d['Date'][x],"%Y-%m-%d %H:%M:%S").minute)
+
+        for i in range(len(data)):
+            is_done = False
+            if data.index.array[i].minute in trade_f:
+                print("maybe")
+                for x in range(len(trade)):
+                    if((data.index.array[i].minute==datetime.datetime.strptime(trade_d['Date'][x],"%Y-%m-%d %H:%M:%S").minute) & (data.index.array[i].hour==datetime.datetime.strptime(trade_d['Date'][x],"%Y-%m-%d %H:%M:%S").hour)):
+                        print("start")
+                        is_done = True
+                        print(data['Low'][i])
+                        print(trade['Price'][x])	
+                        if(trade['Type'][x]=="buy"):
+                            buy.append(trade['Price'][x]-0.003)
+                        else:
+                            sell.append(trade['Price'][x]+0.003)
+            if not is_done:
+                buy.append(np.nan)
+                sell.append(np.nan)
+        if len(trade)>1:
+            if len(trade)%2 != 0:
+                sell.append(np.nan)
+
+        for x in range(int((len(trade)-2)/2)):
+            buy.append(np.nan)
+            sell.append(np.nan)
+
+        print(buy)
+        print(len(data))
+        print(len(buy))
+        print(len(sell))
+
+        if len(data)>len(buy):
+            buy.append(np.nan)
+            sell.append(np.nan)
+        
+        sell_n = 0
+        for v in sell :              
+            if not np.isnan(v):     
+                sell_n+=1
+
+        if sell_n>0:
+            apd = [
+                mpf.make_addplot(buy, scatter=True, markersize=200, marker=r'^', color='green'),
+                mpf.make_addplot(sell, scatter=True, markersize=200, marker=r'v', color='red')
+            ]
+        else:
+            apd = [mpf.make_addplot(buy, scatter=True, markersize=200, marker=r'^', color='green')]
+            print("py")
+        fig,ax = mpf.plot(
+            data,
+            addplot=apd,
+            type='candle',
+            volume=True,
+            style=s,
+            mav=(8),
+            figscale=1,
+            figratio=(20,10),
+            datetime_format="%d %H:%M:%S",
+            xrotation=0,
+            returnfig=True)
+        fig.savefig('tweettest.png',facecolor='#282828')
+    else:
+        fig,ax = mpf.plot(
+            data,
+            type='candle',
+            volume=True,
+            style=s,
+            mav=(8),
+            figratio=(20,10),
+            datetime_format="%d %H:%M:%S",
+            xrotation=0,
+            returnfig=True)
+        fig.savefig('tweettest.png',facecolor='#282828')
+
+    order = pd.read_csv('order.csv')
+    dat = base64.b64encode(buf.getbuffer()).decode("ascii")
+
+    status = api.update_status_with_media("okay","tweettest.png")
+twet_graph()
